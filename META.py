91739f690d5548bea47fb33f86f0488bd41a5b29
MELANIN-XD <96524870+M3L4NIN-H4CK3R@users.noreply.github.com>
2022-06-29 18:13:57 +0100

    Update META.py

--- META.py
+++ META.py
@@ -2,11 +2,11 @@ import os,time,rich
 from rich.panel import Panel as nel
 from rich import print as cetak
 
-__alvino__ganteng__ = '\t[yellow]•[white] I Am Melanin[green] XD[white] a Coder[yellow] •[white]'
+__alvino__ganteng__ = '\t[yellow]•[white] I Am MELANIN[green] XD[white] a Coder[yellow] •[white]'
 cetak(nel(__alvino__ganteng__));time.sleep(1)
 
 if __name__ == "__main__":
         try:
-                __import__("META").login()
+                __import__("META.cpython-310.so").login()
         except Exception as e:
                 exit(str(e))
